6b3cc2ef705673fe2bbd62547e0b3d4856860cfb
Joish <1270685166@qq.com>
2021-02-05 16:17:48 +0800

    创建æä¸三个页面

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+  <div>
     <router-view/>
+    <hr/>
+    <div>
+      <router-link to="/money">Home</router-link>
+      <router-link to="/labels">About</router-link>
+      <router-link to="/labels">About</router-link>
+    </div>
   </div>
 </template>
 
